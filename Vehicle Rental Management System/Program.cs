6f126743dbb248b6911d49ca2a6c792b03a23d4d
Gaganvirdi18 <156145073+Gaganvirdi18@users.noreply.github.com>
2024-03-24 01:35:32 -0400

    Add project files.

---
+++ Vehicle Rental Management System/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating instances of vehicles
+        Vehicle car = new Car("Model S", "Tesla", 2022, 100.00, 4, "Electric", "Automatic", true);
+        Vehicle truck = new Truck("F-150", "Ford", 2020, 150.00, 1000, "Pickup", true);
+        Vehicle motorcycle = new Motorcycle("Ninja 300", "Kawasaki", 2021, 50.00, 300, "Gasoline", true);
+
+        // Displaying vehicle details
+        car.DisplayDetails();
+        truck.DisplayDetails();
+        motorcycle.DisplayDetails();
+
+        // Demonstrating RentalAgency functionality
+        RentalAgency agency = new RentalAgency(3);
+        agency.AddVehicle(car, 0);
+        agency.AddVehicle(truck, 1);
+        agency.AddVehicle(motorcycle, 2);
+
+        agency.RentVehicle(0); // Renting the car
+        Console.WriteLine($"Total revenue: {agency.TotalRevenue}");
+
+        // Trying to rent the car again
+        agency.RentVehicle(0); // Should display "No vehicle available at this index."
+
+        // Removing truck from fleet
+        agency.RemoveVehicle(1);
+    }
+}
+
---
+++ Vehicle Rental Management System/RentalAgency.cs
@@ -0,0 +1,41 @@
+﻿// RentalAgency.cs
+using System;
+
+public class RentalAgency
+{
+    private Vehicle[] Fleet { get; set; }
+    public double TotalRevenue { get; private set; }
+
+    // Constructor
+    public RentalAgency(int fleetSize)
+    {
+        Fleet = new Vehicle[fleetSize];
+    }
+
+    // Method to add vehicles to the fleet
+    public void AddVehicle(Vehicle vehicle, int index)
+    {
+        Fleet[index] = vehicle;
+    }
+
+    // Method to remove vehicles from the fleet
+    public void RemoveVehicle(int index)
+    {
+        Fleet[index] = null;
+    }
+
+    // Method to rent a vehicle
+    public void RentVehicle(int index)
+    {
+        if (Fleet[index] != null)
+        {
+            TotalRevenue += Fleet[index].RentalPrice;
+            Fleet[index] = null;
+            Console.WriteLine("Vehicle rented successfully.");
+        }
+        else
+        {
+            Console.WriteLine("No vehicle available at this index.");
+        }
+    }
+}
